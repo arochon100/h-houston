8d1e64c9d25bb4b394e936fded89a051fa870ac6
antoine rochon <antoine.rochon@hello-houston.com>
2021-07-22 17:29:58 +0200

    page > prestataires

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,20 @@
 class PagesController < ApplicationController
-skip_before_action :authenticate_user!, only: [ :home ]
+skip_before_action :authenticate_user!, only: [ :home, :prestataires ]
 
 
 
 def home
 end
+
+def prestataires
+end
+
+
+
+
+
+
+
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
+  # HOME PAGE 
   root to: 'pages#home'
+
+  # PAGES 
+  get 'prestataires-apprentissages' , to: 'pages#prestataires'
 
 end
